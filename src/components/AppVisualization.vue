72a5daef9a4a68b8da2ede4d2469ab534269a4fb
Diego Pascual <azotillo@gmail.com>
2020-03-24 23:25:17 +0100

    Add vis-network lib Add bootstrap template

--- src/components/AppVisualization.vue
+++ src/components/AppVisualization.vue
@@ -1,19 +1,44 @@
 <template>
-  <div class="title">
-    <h1>Visualization</h1>
-  </div>
+  <h1>Network</h1>
 </template>
 
 <script>
+import vis from "vis-network";
 export default {
     name: 'AppVisualization'
 };
+// create an array with nodes
+  var nodes = new vis.DataSet([
+    {id: 1, label: 'Node 1'},
+    {id: 2, label: 'Node 2'},
+    {id: 3, label: 'Node 3'},
+    {id: 4, label: 'Node 4'},
+    {id: 5, label: 'Node 5'}
+  ]);
+
+  // create an array with edges
+  var edges = new vis.DataSet([
+    {from: 1, to: 3},
+    {from: 1, to: 2},
+    {from: 2, to: 4},
+    {from: 2, to: 5},
+    {from: 3, to: 3}
+  ]);
+
+  // create a network
+  var container = document.getElementById('myNetwork');
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+  var options = {};
+  var network = new vis.Network(container, data, options);
 </script>
 
 <style lang="scss" scoped>
-    .title {
-        h1 {
-            color: red;
-        }
-    }
+    #myNetwork {
+        width: 600px;
+        height: 400px;
+        border: 1px solid lightgray;
+    };
 </style>
