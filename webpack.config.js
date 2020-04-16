67f90c03eb785d737f941f96e236a4f73bd7637a
Daniel Calderón Portillo <daniel.calderon.portillo@gmail.com>
2020-04-16 18:55:24 +0200

    13 # Making some improvements in webpack + including eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,47 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+module.exports = function (webpackEnv) {
+  return {
+    mode: webpackEnv,
+    module: {
+      rules: [
+        {
+          enforce: "pre",
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "eslint-loader",
+          options: {
+            cache: true,
+          },
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html",
+      }),
+      new DuplicatePackageCheckerPlugin(),
+    ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all",
+          },
+        },
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+    },
+  }
 }
