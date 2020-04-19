30bb4b83bb2f2b9d0c591bcec314799580ba21af
Daniel Calderon Portillo <daniel.calderon.portillo@gmail.com>
2020-04-19 10:44:13 +0200

    13 # Remove fake components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Collections from "../Collections"
-import SearchForm from "../SearchForm"
 
 const App = () => {
   return (
     <div className="App">
-      <SearchForm />
       <Collections />
     </div>
   )
--- src/components/News/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const News = () => {
-  return <div className="News">News</div>
-}
-
-export default News
